2cfe99e201df5382fbfe01e67e9dba412979880f
Lucas César Lopes <lucascesarlopes@nagazakisoftware.com.br>
2025-09-08 15:39:47 -0300

    Update driver_on_ride_widget.dart

--- lib/driver_on_ride/driver_on_ride_widget.dart
+++ lib/driver_on_ride/driver_on_ride_widget.dart
@@ -1,11 +1,14 @@
 import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
-import '/flutter_flow/flutter_flow_google_map.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
 import '/custom_code/widgets/index.dart' as custom_widgets;
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:pointer_interceptor/pointer_interceptor.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
 import 'driver_on_ride_model.dart';
 export 'driver_on_ride_model.dart';
 
@@ -53,8 +56,8 @@ class _DriverOnRideWidgetState extends State<DriverOnRideWidget> {
         color: FlutterFlowTheme.of(context).primaryBackground,
         child: Center(
           child: SizedBox(
-            width: 50.0,
-            height: 50.0,
+            width: 50,
+            height: 50,
             child: CircularProgressIndicator(
               valueColor: AlwaysStoppedAnimation<Color>(
                 FlutterFlowTheme.of(context).primary,
@@ -66,7 +69,7 @@ class _DriverOnRideWidgetState extends State<DriverOnRideWidget> {
     }
 
     return StreamBuilder<RideOrdersRecord>(
-      stream: RideOrdersRecord.getDocument(widget.driverOrder!),
+      stream: RideOrdersRecord.getDocument(widget!.driverOrder!),
       builder: (context, snapshot) {
         // Customize what your widget looks like when it's loading.
         if (!snapshot.hasData) {
@@ -74,8 +77,8 @@ class _DriverOnRideWidgetState extends State<DriverOnRideWidget> {
             backgroundColor: FlutterFlowTheme.of(context).tertiary,
             body: Center(
               child: SizedBox(
-                width: 50.0,
-                height: 50.0,
+                width: 50,
+                height: 50,
                 child: CircularProgressIndicator(
                   valueColor: AlwaysStoppedAnimation<Color>(
                     FlutterFlowTheme.of(context).primary,
@@ -95,49 +98,23 @@ class _DriverOnRideWidgetState extends State<DriverOnRideWidget> {
           },
           child: Scaffold(
             key: scaffoldKey,
-            resizeToAvoidBottomInset: false,
             backgroundColor: FlutterFlowTheme.of(context).tertiary,
             body: Stack(
               children: [
-                Opacity(
-                  opacity: 0.0,
-                  child: FlutterFlowGoogleMap(
-                    controller: _model.googleMapsController,
-                    onCameraIdle: (latLng) => _model.googleMapsCenter = latLng,
-                    initialLocation: _model.googleMapsCenter ??=
-                        LatLng(13.106061, -59.613158),
-                    markerColor: GoogleMarkerColor.violet,
-                    mapType: MapType.normal,
-                    style: GoogleMapStyle.standard,
-                    initialZoom: 14.0,
-                    allowInteraction: true,
-                    allowZoom: true,
-                    showZoomControls: true,
-                    showLocation: true,
-                    showCompass: false,
-                    showMapToolbar: false,
-                    showTraffic: false,
-                    centerMapOnMarkerTap: true,
-                    mapTakesGesturePreference: false,
-                  ),
-                ),
-                PointerInterceptor(
-                  intercepting: isWeb,
-                  child: AuthUserStreamWidget(
-                    builder: (context) => Container(
+                AuthUserStreamWidget(
+                  builder: (context) => Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: custom_widgets.TurnByTurnNav(
                       width: double.infinity,
                       height: double.infinity,
-                      child: custom_widgets.TurnByTurnNav(
-                        width: double.infinity,
-                        height: double.infinity,
-                        apiKey: 'AIzaSyCFBfcNHFg97sM7EhKnAP4OHIoY3Q8Y_xQ',
-                        arrivalRadiusMeters: 30.0,
-                        simulateIfNoGPS: false,
-                        useDeviceCompass: false,
-                        userLatLng: driverOnRideRideOrdersRecord.latlngAtual!,
-                        initialDriverLatLng: currentUserDocument?.location,
-                        placeLatLng: driverOnRideRideOrdersRecord.latlng!,
-                      ),
+                      apiKey: 'AIzaSyCFBfcNHFg97sM7EhKnAP4OHIoY3Q8Y_xQ',
+                      arrivalRadiusMeters: 30.0,
+                      simulateIfNoGPS: false,
+                      useDeviceCompass: false,
+                      userLatLng: driverOnRideRideOrdersRecord.latlngAtual!,
+                      initialDriverLatLng: currentUserDocument?.location,
+                      placeLatLng: driverOnRideRideOrdersRecord.latlng!,
                     ),
                   ),
                 ),
